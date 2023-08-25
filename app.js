d343231349e4cf48a2f61debc34e6cd3d75e4723
DEEPAKKUMARG17 <cse221022@saranathan.ac.in>
2023-08-25 14:17:52 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,133 @@
+let btn=document.getElementById("button");
+let subcontent=document.createElement("div");
+let maincontent=document.getElementById("maincontent");
+let image=document.createElement("img");
+let buttonchange=document.getElementById("changetext");
+let text=document.getElementById("text");
+let images=document.getElementById("images");
+let counter=document.getElementById("counter");
+let display=document.getElementById("displayCounter");
+let data=document.getElementById("data");
+let texter=document.getElementById("texter");
+
+let products=document.getElementById("products");
+let productsub=document.createElement("div");
+let username=document.getElementById("username");
+let userpassword=document.getElementById("userpassword");
+let user=document.getElementById("user");
+let password=document.getElementById("pass")
+let logout=document.getElementById("logout");
+let adder=document.getElementById("adder")
+let tiltlecart=document.getElementById("titlecart")
+let detailcart=document.getElementById("detailscart");
+
+let view=document.getElementById("view");
+
+let arr=[{prname:"phone",details:"used to phone people"},
+{prname:"car",details:"used to drive"},
+{prname:"bat",details:"used play"}
+]
+
+adder.onclick=()=>
+{
+    popup.style.display="block";
+    popupbg.style.display="block";
+}
+logout.onclick=()=>
+{
+    user.innerHTML=""
+    password.innerHTML=""
+
+}
+
+function append(prname,details)
+{
+let obj={prname,details}
+arr.push(obj)
+console.log(arr)
+}
+function addcart(event)
+{
+    event.preventDefault()
+let prname=tiltlecart.value;
+let details=detailcart.value;
+append(prname,details);
+show();
+popup.style.display="none";
+popupbg.style.display="none";
+
+}
+function shower(event)
+{
+    event.preventDefault();
+   user.innerHTML=username.value;
+   password.innerHTML=userpassword.value
+}
+
+function show()
+{
+    arr.forEach((val)=>
+    {
+        let productpara=document.createElement("p")
+let productdescription=document.createElement("p")
+    productpara.innerHTML = val.prname;
+    productdescription.innerHTML=val.details;
+    let productviewbutton=document.createElement("button");
+    productviewbutton.innerHTML = "showproduct";
+    productviewbutton.onclick="dispalyer()"
+    productsub.appendChild(productpara)
+    productsub.appendChild(productdescription);
+    productsub.appendChild(productviewbutton);
+    view.appendChild(productsub)
+       
+    })
+}
+// view.onclick=(arr)=>
+// {
+    
+// }
+
+function displayer()
+{
+    
+}
+btn.onclick=()=>
+{
+    let url = `https://restcountries.com/v3.1/all`;
+    fetch(url).then((response) => response.json()).then((response) => {
+     for(var i=0;i<8;i++)
+     {
+        let image=document.createElement("img");
+        image.src = response[i].flags.png;
+        subcontent.appendChild(image);
+        maincontent.appendChild(subcontent);
+     }
+     
+    });
+  
+
+}
+buttonchange.onclick=()=>{
+    if(text.innerHTML==='ClickMe')
+    {
+    
+    text.innerHTML="clicked"
+    }
+    else
+    {
+        text.innerHTML="ClickMe"
+    }
+
+
+}
+let count=0
+counter.onclick=()=>
+{
+    count++;
+   display.innerHTML=`Count: ${count}`
+}
+
+data.onkeyup=()=>
+{
+    texter.innerHTML=data.value
+}
